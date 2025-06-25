fac198b147e3acbfb6720a2bcca42235f909f0d1
Andrii Iakubovych <andrii.iakubovych@gmail.com>
2025-06-25 15:16:21 +0200

    Add AppDbContextFactory

---
+++ ImageApi/Infrastructure/Persistence/AppDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ImageApi.Infrastructure.Persistence
+{
+	public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+	{
+		public AppDbContext CreateDbContext(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: true)
+				.AddEnvironmentVariables()
+				.Build();
+
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+			if (string.IsNullOrEmpty(connectionString))
+			{
+				throw new InvalidOperationException("DefaultConnection string is not configured.");
+			}
+
+			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+			optionsBuilder.UseNpgsql(connectionString);
+
+			return new AppDbContext(optionsBuilder.Options);
+		}
+	}
+}
