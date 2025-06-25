9e9f36c99393a069ea43fd1f90a3f5134e1f147f
Andrii Iakubovych <andrii.iakubovych@gmail.com>
2025-06-25 17:03:23 +0200

    Recovered AppDbContextFactory

---
+++ ImageApi/Infrastructure/Persistence/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ImageApi.Infrastructure.Persistence
+{
+	public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+	{
+		public AppDbContext CreateDbContext(string[] args)
+		{
+			var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
+				?? throw new InvalidOperationException("DB_CONNECTION_STRING is not configured.");
+
+			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+			optionsBuilder.UseNpgsql(connectionString);
+
+			return new AppDbContext(optionsBuilder.Options);
+		}
+	}
+}
